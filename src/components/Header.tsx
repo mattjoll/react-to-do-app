b73afddc671e77ceddc159934b45b65d8a098cdd
Matthew Joll <dev@mattjoll.nz>
2021-05-19 22:46:30 +1200

    [main] Tidy up, set up and add header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,8 @@
+import { FunctionComponent } from "react";
+import './Header.css';
+
+export const Header: FunctionComponent = () => {
+    return (
+        <header className="header">Tasks</header>
+    );
+}
